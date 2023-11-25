bcff43882330fadd47b36883a7964630ba23b445
Ted2121 <andrei.teodor1372@gmail.com>
2023-11-25 20:03:48 +0100

    still not good

--- marketshuffle/src/items/PositionsChart.jsx
+++ marketshuffle/src/items/PositionsChart.jsx
@@ -1,6 +1,5 @@
 import * as React from 'react';
 import Box from '@mui/material/Box';
-
 import { LineChart } from '@mui/x-charts/LineChart';
 
 const test = [
@@ -14,12 +13,46 @@ const test = [
         date: new Date(2023, 3, 24),
         details: ""
     }
-]
+];
 
 const costs = test.map(item => item.cost);
 const dates = test.map(item => item.date);
 
-export default function PositionsChart({positions}) {
+export default function PositionsChart({ positions }) {
+    const timeData = dates.map(date => date); // Convert dates to milliseconds
+
+    const costData = dates.map((date, index) => ({
+        x: date.getTime(), // x value represents time in milliseconds
+        y: costs[index],   // y value is the corresponding cost
+    }));
+
+    const valueFormatter = (date) =>
+        date.toLocaleDateString('en-EN', {
+            month: '2-digit',
+            day: '2-digit',
+        });
+
+    const config = {
+        series: [
+            { data: costData },
+        ],
+        height: 300,
+        bottomAxis: 'bottomAxis',
+        rightAxis: 'rightAxis',
+        leftAxis: null,
+    };
+
+    const xAxisCommon = {
+        data: timeData,
+        scaleType: 'time',
+        valueFormatter,
+    };
+
+    const yAxisCommon = {
+        data: [], // Update this with appropriate y-axis data if needed
+        scaleType: 'linear',
+    };
+
     return (
         <Box sx={{ width: 500, maxWidth: 500 }}>
             <LineChart
@@ -27,14 +60,14 @@ export default function PositionsChart({positions}) {
                     {
                         ...xAxisCommon,
                         id: 'bottomAxis',
-                        scaleType: 'point',
+                        scaleType: 'time',
                     },
                 ]}
                 yAxis={[
                     {
                         ...yAxisCommon,
                         id: 'rightAxis',
-                        scaleType: 'point'
+                        scaleType: 'linear'
                     }
                 ]}
                 {...config}
@@ -42,42 +75,3 @@ export default function PositionsChart({positions}) {
         </Box>
     );
 }
-
-const valueFormatter = (date) =>
-    date.toLocaleDateString('en-EN', {
-        month: '2-digit',
-        day: '2-digit',
-    });
-
-const timeData = [
-    ...dates
-];
-
-const costData = [
-   ...costs
-];
-
-
-const showMark = (params) => {
-    const { position } = params;
-    return position.getHours() === 0;
-};
-
-const config = {
-    series: [
-        { data: costData, showMark },
-    ],
-    height: 300,
-    bottomAxis: 'bottomAxis',
-    rightAxis: 'rightAxis',
-    leftAxis: null,
-};
-const xAxisCommon = {
-    data: timeData,
-    scaleType: 'time',
-    valueFormatter,
-};
-const yAxisCommon = {
-    data: costData,
-    scaleType: 'linear',
-};
