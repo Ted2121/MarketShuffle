37e1bf477e6884fb10adcca4c4c67b463a43dbca
Ted2121 <andrei.teodor1372@gmail.com>
2023-11-25 16:31:46 +0100

    fwefwe

--- marketshuffle/src/items/ActionsPanel.jsx
+++ marketshuffle/src/items/ActionsPanel.jsx
@@ -1,43 +1,83 @@
-import { Box } from '@mui/material'
-import React from 'react'
+import { Box, Button, Card, Typography } from '@mui/material'
+import React, { useState } from 'react'
 
-function ActionsPanel({item}) {
+function ActionsPanel({ item }) {
+  const [currentPosition, setCurrentPosition] = useState(null);
+
+  const handleSetCurrentPosition = () => {
+    setCurrentPosition()
+  }
+
+  const positionList = item?.positions.map((position) => (
+    <Button onClick={handleSetCurrentPosition}>
+      {position.cost}
+    </Button>
+  ))
+
   return (
     <Box sx={{
-        display: 'flex',
-        flex:70,
-        justifyContent: 'center',
-        alignItems: 'center',
+      display: 'flex',
+      flex: 70,
+      justifyContent: 'center',
+      alignItems: 'center',
     }}>
-        {/* price history */}
+      {/* price history */}
+      <Box>
+        {/* Positions */}
         <Box>
-            {/* Positions */}
-            <Box>
-
-            </Box>
-            {/* Position details */}
-            <Box>
-
-            </Box>
-            {/* graph */}
-            <Box>
-
-            </Box>
+          <Card>
+            {positionList}
+          </Card>
         </Box>
-        {/* recipe */}
-        {/* Kitsou hair: 8x [   ]  = x kamas*/}
-        {/* x : 18x [   ]  = x kamas*/}
-        {/* y: 2x [   ]  = x kamas*/}
-        {/* Unmaged Item price: [   ] kamas - Profit: x kamas*/}
-        {/* Ok Item price: [   ] kamas - Profit: x kamas */}
-        {/* Perfect Item price: [   ] kamas - Profit: x kamas */}
+        {/* Position details */}
         <Box>
+          <Card>
+            <Box>
+              <Typography>
+                Cost:
+              </Typography>
+              <Typography>
+                {currentPosition?.cost}
+              </Typography>
+            </Box>
+            <Box>
+              <Typography>
+                Date:
+              </Typography>
+              <Typography>
+                {currentPosition?.date}
+              </Typography>
+            </Box>
+            <Box>
+              <Typography>
+                Details:
+              </Typography>
+              <Typography>
+                {currentPosition?.details}
+              </Typography>
+            </Box>
+          </Card>
+        </Box>
+        {/* graph */}
 
-        </Box>
-        {/* Add item */}
         <Box>
-            
+        
         </Box>
+      </Box>
+      {/* recipe */}
+      {/* Kitsou hair: 8x [   ]  = x kamas*/}
+      {/* x : 18x [   ]  = x kamas*/}
+      {/* y: 2x [   ]  = x kamas*/}
+      {/* Unmaged Item price: [   ] kamas - Profit: x kamas*/}
+      {/* Ok Item price: [   ] kamas - Profit: x kamas */}
+      {/* Perfect Item price: [   ] kamas - Profit: x kamas */}
+      <Box>
+
+      </Box>
+      {/* Add item */}
+      <Box>
+
+      </Box>
     </Box>
   )
 }
