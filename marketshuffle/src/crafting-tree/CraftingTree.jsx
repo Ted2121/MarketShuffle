c40b73d8aea070c2b49a8e00339352fde2e14d53
Ted2121 <andrei.teodor1372@gmail.com>
2024-01-26 20:18:37 +0100

    looking good

--- marketshuffle/src/crafting-tree/CraftingTree.jsx
+++ marketshuffle/src/crafting-tree/CraftingTree.jsx
@@ -1,27 +1,45 @@
 import React from 'react';
 import item from '../data/mocks/item-mock';
-import { Box } from '@mui/material';
+import { Box, Checkbox, Typography } from '@mui/material';
 import RecipeTier from './RecipeTier';
+import { TreeView } from '@mui/x-tree-view/TreeView';
+import { TreeItem } from '@mui/x-tree-view/TreeItem';
+import { v4 as uuidv4 } from 'uuid';
 
 function CraftingTree() {
-
     const recipe = item.recipe;
-// console.log(recipe)
-    const renderTiers = (data, depth = 0) => {
-        console.log(data)
-        return (
-          <Box key={depth} sx={{ display: 'flex', flexDirection: 'column', gap: 2 }}>
-            {Array.isArray(data) && data.map((item, index) => (
-              <React.Fragment key={index}>
-                <RecipeTier tierData={data} />
-                {item.recipe && renderTiers(item.recipe, depth + 1)}
-              </React.Fragment>
-            ))}
-          </Box>
-        );
-      };
-    
-      return <Box>{renderTiers(recipe)}</Box>;
-}
+  
+    const renderTree = (data) => {
+      const nodeId = uuidv4();
+      return (
+        <TreeItem key={nodeId} nodeId={nodeId} label={renderLabel(data)}>
+          {Array.isArray(data.recipe) ? data.recipe.map((subItem) => renderTree(subItem)) : null}
+        </TreeItem>
+      );
+    };
+  
+    const renderLabel = (data) => (
+      <Box sx={{ display: 'flex', alignItems: 'center' }}>
+        <Typography sx={{ fontSize: '1.5rem', marginRight: 1 }}>{data.name}:</Typography>
+        <Checkbox size="large" />
+      </Box>
+    );
+  
+    // Create a root node with a label for the entire tree
+    const rootNode = (
+      <TreeItem key="root" nodeId="root" label={<Typography variant="h5">Crafting Recipes:</Typography>}>
+        {recipe.map((item) => renderTree(item))}
+      </TreeItem>
+    );
+  
+    return (
+      <TreeView sx={{ height: 240, flexGrow: 1, maxWidth: 400, overflowY: 'auto' }}>
+        {rootNode}
+      </TreeView>
+    );
+  }
+  
+  
+  
 
 export default CraftingTree
