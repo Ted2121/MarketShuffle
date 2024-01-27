d9632df72302bcefc5050cbd8bf90c83c9cf31fd
Ted2121 <andrei.teodor1372@gmail.com>
2024-01-27 06:03:29 +0100

    tree

--- marketshuffle/src/crafting-tree/CraftingTree.jsx
+++ marketshuffle/src/crafting-tree/CraftingTree.jsx
@@ -5,30 +5,36 @@ import RecipeTier from './RecipeTier';
 import { TreeView } from '@mui/x-tree-view/TreeView';
 import { TreeItem } from '@mui/x-tree-view/TreeItem';
 import { v4 as uuidv4 } from 'uuid';
+import { ExpandMore, ChevronRight } from '@mui/icons-material';
 
 function CraftingTree() {
   const recipe = item.recipe;
 
   // Generate unique IDs for each node
-  const generateNodeIds = (data) => {
+  const generateNodeIds = (data, ids = []) => {
     const nodeId = uuidv4();
+    ids.push(nodeId);
+
     if (Array.isArray(data.recipe)) {
-      data.recipe.forEach((subItem) => generateNodeIds(subItem));
+      data.recipe.forEach((subItem) => generateNodeIds(subItem, ids));
     }
-    return nodeId;
+
+    return ids;
   };
 
   // Generate IDs for all nodes
-  const expandedNodes = recipe.map((item) => generateNodeIds(item));
+  const expandedNodes = generateNodeIds({ recipe });
 
   const renderTree = (data) => {
     const nodeId = uuidv4();
-
+  
     return (
       <TreeItem
         key={nodeId}
         nodeId={nodeId}
         label={renderLabel(data)}
+        defaultCollapseIcon={<ExpandMore />} // Set default collapse icon for the TreeItem
+        defaultExpandIcon={<ChevronRight />} // Set default expand icon for the TreeItem
       >
         {Array.isArray(data.recipe) ? data.recipe.map((subItem) => renderTree(subItem)) : null}
       </TreeItem>
@@ -46,12 +52,12 @@ function CraftingTree() {
     <TreeView
       sx={{ height: 240, flexGrow: 1, maxWidth: 400, overflowY: 'auto' }}
       defaultExpanded={expandedNodes}
+      defaultCollapseIcon={<ExpandMore />} // Set default collapse icon
+      defaultExpandIcon={<ChevronRight />} // Set default expand icon
     >
       {recipe.map((item) => renderTree(item))}
     </TreeView>
   );
 }
-
-  
 
 export default CraftingTree
