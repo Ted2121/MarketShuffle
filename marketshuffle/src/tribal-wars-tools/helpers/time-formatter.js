abd893ae79e6db37b1e04d117dfd4bc90e0da4c4
Ted2121 <andrei.teodor1372@gmail.com>
2024-10-20 12:37:12 +0200

    managed to extract both date and link

--- marketshuffle/src/tribal-wars-tools/helpers/time-formatter.js
+++ marketshuffle/src/tribal-wars-tools/helpers/time-formatter.js
@@ -6,9 +6,11 @@ export function parseCustomDate(input, minutesOffset = 0) {
     const tomorrow = new Date(today);
     tomorrow.setDate(today.getDate() + 1); // Set tomorrow's date
 
+    // Regular expressions
     const regexDateTime = /(\d{1,2})\.(\d{1,2})\. la ora (\d{1,2}):(\d{1,2})(?::(\d{1,2})(?::(\d{1,3}))?)?/; // Includes optional seconds and milliseconds
     const regexTomorrow = /mâine la ora (\d{1,2}):(\d{1,2})(?::(\d{1,2})(?::(\d{1,3}))?)?/; // Includes optional seconds and milliseconds
     const regexToday = /astăzi la ora (\d{1,2}):(\d{1,2})(?::(\d{1,2})(?::(\d{1,3}))?)?/; // Includes optional seconds and milliseconds
+    const reportDateFormat = /^(ian\.|feb\.|mar\.|apr\.|mai\.|iun\.|iul\.|aug\.|sep\.|oct\.|noi\.|dec\.) (\d{1,2}), (\d{1,2}):(\d{1,2})$/; // Format: oct. 20, 12:47
 
     let match;
 
@@ -44,6 +46,31 @@ export function parseCustomDate(input, minutesOffset = 0) {
 
         // Set the time to today
         return new Date(today.getFullYear(), today.getMonth(), today.getDate(), hours - 1, minutes - minutesOffset, seconds, milliseconds);
+    }
+
+    // Check for "oct. 20, 12:47"
+    if ((match = reportDateFormat.exec(input))) {
+        const monthMap = {
+            ian: 0,
+            feb: 1,
+            mar: 2,
+            apr: 3,
+            mai: 4,
+            iun: 5,
+            iul: 6,
+            aug: 7,
+            sep: 8,
+            oct: 9,
+            noi: 10,
+            dec: 11,
+        };
+
+        const month = monthMap[match[1].slice(0, 3)];
+        const day = parseInt(match[2], 10);
+        const hours = parseInt(match[3], 10);
+        const minutes = parseInt(match[4], 10);
+
+        return new Date(today.getFullYear(), month, day, hours - 1, minutes - minutesOffset);
     }
 
     return input;
