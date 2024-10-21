511bab9379f82af62b9e97f4308b4f812c26277f
Ted2121 <andrei.teodor1372@gmail.com>
2024-10-21 19:02:11 +0200

    added export button

--- marketshuffle/src/shared/layouts/Navbar.jsx
+++ marketshuffle/src/shared/layouts/Navbar.jsx
@@ -6,6 +6,7 @@ import Container from '@mui/material/Container';
 import Button from '@mui/material/Button';
 import navData from '../../data/components-text/navData';
 import { Link } from 'react-router-dom';
+import ExportButton from '../../import-export/export-button';
 
 const pages = navData;
 
@@ -14,24 +15,27 @@ function Navbar() {
         <AppBar position="static">
             <Container maxWidth="xl">
                 <Toolbar disableGutters>
-                    <Box sx={{ flexGrow: 1, display: 'flex' }}>
-                        {pages.map((page, index) => (
-                            <Link 
-                            key={index}
-                            to={page.route} style={{ textDecoration: 'none' }}>
-                                <Button
-                                    key={page.id}
-                                    sx={{
-                                        my: 2, color: 'white.main',
-                                        fontSize: '1.1rem',
-                                        marginRight: '16px'
-                                    }}
-                                >
-                                    {page.text}
-                                </Button>
-                            </Link>
-                        ))}
-                    </Box>
+                        <Box sx={{ flexGrow: 1, display: 'flex' }}>
+                            {pages.map((page, index) => (
+                                <Link
+                                    key={index}
+                                    to={page.route} style={{ textDecoration: 'none' }}>
+                                    <Button
+                                        key={page.id}
+                                        sx={{
+                                            my: 2, color: 'white.main',
+                                            fontSize: '1.1rem',
+                                            marginRight: '16px'
+                                        }}
+                                    >
+                                        {page.text}
+                                    </Button>
+                                </Link>
+                            ))}
+                        </Box>
+                        <Box>
+                            <ExportButton />
+                        </Box>
                 </Toolbar>
             </Container>
         </AppBar>
